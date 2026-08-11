af90a9271130013e74fde02219c84e21a6cbcd6a
Himanshu Dubal <himanshudubal778@gmail.com>
2026-08-11 11:22:26 +0530

    added the CORS configuration for cross origin platform and added the WebSocket for sending the notification real time to user

---
+++ src/main/java/com/banking/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.banking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				//.allowedOrigins("http://localhost:5173", "https://upfixwmxmrpl6.ok.kimi.link","http://localhost:3000")
+				.allowedMethods("GET","PUT,","POST","DELETE","OPTIONS","PATCH")
+				.allowedHeaders("*")
+				.exposedHeaders("Authorization")
+				.allowCredentials(true);
+				//.maxAge(3600);
+			}
+		};
+	}
+
+}
