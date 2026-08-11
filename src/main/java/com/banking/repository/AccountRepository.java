2b0857a7d757c548e577b74b25e8c12740480ae1
Himanshu Dubal <himanshudubal778@gmail.com>
2026-08-11 11:17:30 +0530

    made a change in Account Repository

--- src/main/java/com/banking/repository/AccountRepository.java
+++ src/main/java/com/banking/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package com.banking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import com.banking.model.User;
 import java.util.List;
 import java.util.Optional;
 
-
+@EnableJpaRepositories
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
@@ -22,6 +23,6 @@ public interface AccountRepository extends JpaRepository<Account, Long>{
 	@Query("SELECT COUNT(a) FROM Account a where a.user = :user")
 	long countByUser(@Param("user") User user);
 	
-	@Query("SELECT a FROM Account a.user = :user AND a.isActive = true")
+	@Query("SELECT a FROM Account a WHERE a.user = :user AND a.isActive = true")
 	List<Account> findActiveAccountByUser(@Param("user") User user);
 }
